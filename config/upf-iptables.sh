f87456d6a7b64af396aa239a4cbc509465691ecd
kkaras <konradkar2@gmail.com>
2022-10-16 18:26:25 +0200

    add seperate docker for UE, add image for iperf

--- config/upf-iptables.sh
+++ config/upf-iptables.sh
@@ -2,6 +2,21 @@
 #
 # Configure iptables in UPF
 #
-iptables -t nat -A POSTROUTING -o eth0  -j MASQUERADE
-iptables -I FORWARD 1 -j ACCEPT
+
+ip address show dev eth0 | grep 10.100.201.3
+
+dnnIfc=""
+
+retVal=$?
+if [ $retVal -eq 0 ]; then
+    echo "eth0 is dnn"
+    dnnIfc="eth0"
+else
+    echo "eth1 is dnn"
+    dnnIfc="eth1"
+fi
+
+iptables -t nat -A POSTROUTING -s 10.60.0.0/24 -o "$dnnIfc"  -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.61.0.0/24 -o "$dnnIfc"  -j MASQUERADE
+
 
